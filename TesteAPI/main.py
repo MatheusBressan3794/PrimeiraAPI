91e8456a67ce568010603bd00c06eb5abfdc8ae5
Matheus Bressan Vila Nova <matheusbressan3794@gmail.com>
2026-04-27 09:30:26 -0300

    Terminando a API de livros

--- TesteAPI/main.py
+++ TesteAPI/main.py
@@ -30,5 +30,28 @@ def obter_livro_por_id():
         if livro.get('id') == id:
             return jsonify(livro)
 
+@app.route('/livros/<int:id>', methods=['PUT']
+def editar_livro_por_id(id):
+    livro_alterado = request.get_json()
+    for indice,livro in enumerate(livros):
+        if livro.get('id) == id:
+                     livro[indice].update(livro_alternado)
+                     return jsonify(livroe[indicie])
+
+@app.route('/livros', methods=['POST'])
+def incluir_novo_livro():
+
+    novo_livro = request.get_json()
+    livros.append(novo_livro)
+
+    return jsonify(livros)
+
+@app.route('/livros/<int:id>', methods=['DELETE']
+def excluir_livro(id):
+    for indice, livro in inumerate(livros):
+        if livros.get('id') == id:
+            del livros[indice]
+
+    return jsonify(livros)
 
 app.run(port=5000,host='localhost',debug=True)
